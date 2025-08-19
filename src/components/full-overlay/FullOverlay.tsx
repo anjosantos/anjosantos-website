5be8412d73d5df6a5acb8bb728280c9461245754
Anjo Santos <anjosantos92@gmail.com>
2025-08-19 15:31:35 -0700

    Added draft overlay

--- src/components/full-overlay/FullOverlay.tsx
+++ src/components/full-overlay/FullOverlay.tsx
@@ -1,5 +1,12 @@
 import { usePage } from "@/providers";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faSquareLinkedin,
+  faSquareGithub,
+  faSquareInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+
 import { PageKeys } from "@/pages/grid-pages";
 import { FullOverlayContainer, OverlaySection } from "@/components";
 
@@ -17,7 +24,6 @@ const FullOverlay: React.FC<FullOverlayProps> = () => {
     right: 0,
     bottom: 0,
     backgroundColor: "transparent",
-    color: "white",
     width: "100vw",
     height: "100vh",
     zIndex: 1,
@@ -40,10 +46,82 @@ const FullOverlay: React.FC<FullOverlayProps> = () => {
     <section style={overlayStyle}>
       {
         <section>
-          <FullOverlayContainer width={300} position={1}>
+          <FullOverlayContainer width={210} position={1}>
             <OverlaySection>
-              <b>TEST</b>
+              <span
+                style={{
+                  fontFamily: "SpaceAge",
+                  color: "#30de3c",
+                  textDecoration: "underline",
+                  display: "block",
+                }}
+              >
+                ANJOSANTOS.DEV
+              </span>
+              <span
+                style={{
+                  fontFamily: "DsDigital",
+                  color: "#8cd1c4",
+                }}
+              >
+                FRONTEND DEVELOPER
+              </span>
             </OverlaySection>
+          </FullOverlayContainer>
+          <FullOverlayContainer width={210} position={4}>
+            <OverlaySection>
+              <span className="overlay-label">PAGES</span>
+
+              <section className="pages-options">
+                <span className="pages-option active">01</span>
+                <span className="pages-option">02</span>
+                <span className="pages-option">03</span>
+                <span className="pages-option">04</span>
+              </section>
+            </OverlaySection>
+            <section className="two-columns-flex">
+              <section className="column">
+                <OverlaySection>
+                  <span className="overlay-label">OXY</span>
+                  <span className="overlay-value active">96%</span>
+                </OverlaySection>
+              </section>
+              <section className="column">
+                <OverlaySection>
+                  <span className="overlay-label">TEMP</span>
+                  <span className="overlay-value">2.7K</span>
+                </OverlaySection>
+              </section>
+            </section>
+            <OverlaySection>
+              <section className="two-columns-flex">
+                <section className="column">
+                  <span className="overlay-label">ORIGIN</span>
+                </section>
+                <section className="column" style={{ flex: 1.2 }}>
+                  <span className="overlay-value">CANADA</span>
+                </section>
+              </section>
+            </OverlaySection>
+            <section className="two-columns-flex">
+              <section className="column" style={{ flex: 1.6 }}>
+                <OverlaySection>
+                  <span className="overlay-label">STATUS</span>
+                  <span className="overlay-value active">ONLINE</span>
+                </OverlaySection>
+              </section>
+              <section className="column">
+                <OverlaySection>
+                  <span className="overlay-label">LONG 93E</span>
+                  <span className="overlay-label">LAT 11N</span>
+                </OverlaySection>
+              </section>
+            </section>
+            <section style={{ marginTop: 5 }}>
+              <FontAwesomeIcon icon={faSquareLinkedin} color="#8cd1c4" size="lg" />
+              <FontAwesomeIcon icon={faSquareGithub} color="#8cd1c4" size="lg" />
+              <FontAwesomeIcon icon={faSquareInstagram} color="#8cd1c4" size="lg" />
+            </section>
           </FullOverlayContainer>
         </section>
       }
--- src/components/overlay-section/OverlaySection.tsx
+++ src/components/overlay-section/OverlaySection.tsx
@@ -4,8 +4,14 @@ const OverlaySection: React.FC<{
 }> = ({ children }) => {
   const overlaySectionStyle = {
     backgroundColor: "rgba(49, 77, 61, 0.3)",
+    boxSizing: "border-box",
     padding: 10,
     color: "white",
+    marginTop: 5,
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
   } as React.CSSProperties;
 
   return <section style={overlaySectionStyle}>{children}</section>;
