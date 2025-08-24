b0e39708bac7ae3ff85aff8f85475dbcd217d678
Anjo Santos <anjosantos92@gmail.com>
2025-08-23 23:59:59 -0700

    Create Contact Page

--- src/pages/grid-pages/contact/Contact.tsx
+++ src/pages/grid-pages/contact/Contact.tsx
@@ -1,13 +1,32 @@
 import React from "react";
 
-import { GridPage } from "@/components";
+import { GridPage, FloatingWrapper } from "@/components";
+
+import "./contact.css";
 
 type ContactProps = {};
 
 const Contact: React.FC<ContactProps> = () => {
   return (
     <GridPage>
-      <section></section>
+      <section className="contact-main-container">
+        <section className="contact-container">
+          <FloatingWrapper>
+            <h1 className="contact-heading">Greetings!</h1>
+            <span className="contact-description">
+              I am not on Earth right now, but you can still reach me at this
+              email:{" "}
+              <a
+                className="contact-email transition clickable"
+                href="mailto:santos.nicol.angelo@gmail.com"
+              >
+                santos.nicol.angelo@gmail.com
+              </a>
+              .
+            </span>
+          </FloatingWrapper>
+        </section>
+      </section>
     </GridPage>
   );
 };
