1ca153082b35cebd838ba86fd902411eeec1e5de
Anjo Santos <anjosantos92@gmail.com>
2025-08-27 13:40:15 -0700

    Revert "Add test label"
    This reverts commit f4fb85529c94c72d291bba5acebc999041f4a1e4.

--- src/components/full-overlay/FullOverlay.tsx
+++ src/components/full-overlay/FullOverlay.tsx
@@ -22,7 +22,6 @@ const FullOverlay: React.FC<FullOverlayProps> = () => {
     <section className="full-overlay-container">
       {
         <section>
-          <b>TESTTTT</b>
           <div
             style={{
               position: "absolute",
