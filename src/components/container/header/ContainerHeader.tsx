1b6c685846e872b775821f9733e1f0f4c012faac
Anjo Santos <anjosantos92@gmail.com>
2025-08-27 13:07:04 -0700

    Fix mobile view projects

--- src/components/container/header/ContainerHeader.tsx
+++ src/components/container/header/ContainerHeader.tsx
@@ -1,19 +1,10 @@
+import "./container-header.css";
+
 type ContainerHeaderProps = {
   label: string;
 };
 const ContainerHeader: React.FC<ContainerHeaderProps> = ({ label }) => {
-  return (
-    <h5
-      style={{
-        margin: 0,
-        fontFamily: "D3Digitalism",
-        fontSize: 30,
-        color: "#ffcb6a",
-      }}
-    >
-      {label}
-    </h5>
-  );
+  return <h5 className="container-header">{label}</h5>;
 };
 
 export default ContainerHeader;
--- src/components/container/paragraph/ContainerParagraph.tsx
+++ src/components/container/paragraph/ContainerParagraph.tsx
@@ -1,19 +1,10 @@
+import "./container-paragraph.css";
+
 type ContainerParagraphProps = {
   body: string;
 };
 const ContainerParagraph: React.FC<ContainerParagraphProps> = ({ body }) => {
-  return (
-    <p
-      style={{
-        margin: 20,
-        fontFamily: "DsDigital",
-        fontSize: 20,
-        color: "#f9e9b6",
-      }}
-    >
-      {body}
-    </p>
-  );
+  return <p className="container-paragraph">{body}</p>;
 };
 
 export default ContainerParagraph;
