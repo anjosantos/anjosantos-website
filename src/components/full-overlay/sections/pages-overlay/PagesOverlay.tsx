ae7ea81d08bda0239ab4e4cf5511296afaf82db4
Anjo Santos <anjosantos92@gmail.com>
2025-08-27 14:01:38 -0700

    Fix height issue on mobile

--- src/components/full-overlay/sections/pages-overlay/PagesOverlay.tsx
+++ src/components/full-overlay/sections/pages-overlay/PagesOverlay.tsx
@@ -1,5 +1,6 @@
 import { PageKeys } from "@/pages/grid-pages";
 import { OverlaySection } from "@/components";
+import { motion, AnimatePresence } from "framer-motion";
 
 type PagesOverlayContainerProps = {
   activeKey: PageKeys;
@@ -11,50 +12,60 @@ const PagesOverlayContainer: React.FC<PagesOverlayContainerProps> = ({
   isVisible,
 }) => {
   return (
-    <section className={`transition hidable ${isVisible ? "visible" : ""}`}>
-      <OverlaySection>
-        <span className="overlay-label">PAGES</span>
+    <AnimatePresence>
+      {isVisible && (
+        <motion.section
+          className="hidable"
+          initial={{ opacity: 0, y: 10 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -10 }}
+          transition={{ duration: 0.3 }}
+        >
+          <OverlaySection>
+            <span className="overlay-label">PAGES</span>
 
-        <section className="pages-options">
-          <span
-            className={`${
-              activeKey === PageKeys.HOME
-                ? "pages-option active green-glow-text"
-                : "pages-option"
-            }`}
-          >
-            01
-          </span>
-          <span
-            className={`${
-              activeKey === PageKeys.PROJECTS
-                ? "pages-option active green-glow-text"
-                : "pages-option"
-            }`}
-          >
-            02
-          </span>
-          <span
-            className={`${
-              activeKey === PageKeys.ABOUT
-                ? "pages-option active green-glow-text"
-                : "pages-option"
-            }`}
-          >
-            03
-          </span>
-          <span
-            className={`${
-              activeKey === PageKeys.CONTACT
-                ? "pages-option active green-glow-text"
-                : "pages-option"
-            }`}
-          >
-            04
-          </span>
-        </section>
-      </OverlaySection>
-    </section>
+            <section className="pages-options">
+              <span
+                className={`${
+                  activeKey === PageKeys.HOME
+                    ? "pages-option active green-glow-text"
+                    : "pages-option"
+                }`}
+              >
+                01
+              </span>
+              <span
+                className={`${
+                  activeKey === PageKeys.PROJECTS
+                    ? "pages-option active green-glow-text"
+                    : "pages-option"
+                }`}
+              >
+                02
+              </span>
+              <span
+                className={`${
+                  activeKey === PageKeys.ABOUT
+                    ? "pages-option active green-glow-text"
+                    : "pages-option"
+                }`}
+              >
+                03
+              </span>
+              <span
+                className={`${
+                  activeKey === PageKeys.CONTACT
+                    ? "pages-option active green-glow-text"
+                    : "pages-option"
+                }`}
+              >
+                04
+              </span>
+            </section>
+          </OverlaySection>
+        </motion.section>
+      )}
+    </AnimatePresence>
   );
 };
 
